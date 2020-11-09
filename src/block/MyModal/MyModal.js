390cdfbcaf91756f9589b1af5811ef9ec9c73072
tuanfront_end <atuanvptd@gmail.com>
2020-11-09 14:11:00 +0700

    change data atributes block guternberg

--- src/block/MyModal/MyModal.js
+++ src/block/MyModal/MyModal.js
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import { Button, Modal } from '@wordpress/components'; 
+import { useState } from 'react';
+import { Button, Modal } from '@wordpress/components';
 
-const MyModal = ({onCloseModal, children}) => {
-
-    return (
-        <div className="wilMyModal">
-            <Modal onRequestClose={ onCloseModal } overlayClassName="wilMyModal__overlay" shouldCloseOnClickOutside={false}>
-              { children }
-            </Modal>
-        </div>
-    )
-}
+const MyModal = ({ onCloseModal, children }) => {
+  return (
+    <div className="wilMyModal">
+      <Modal onRequestClose={onCloseModal} overlayClassName="wilMyModal__overlay" shouldCloseOnClickOutside={false}>
+        {children}
+      </Modal>
+    </div>
+  );
+};
 
 export default MyModal;
