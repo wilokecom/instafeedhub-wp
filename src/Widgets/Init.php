d83f4005dcc6c159df49860a8312b215b82265e5
unknown <phucngocdo@gmail.com>
2020-11-12 12:10:12 +0700

    add and delete widget action

--- src/Widgets/Init.php
+++ src/Widgets/Init.php
@@ -10,11 +10,30 @@ class Init
 {
 	public function __construct()
 	{
+
 		add_action('widgets_init', [$this, 'init']);
+		add_action('delete_widget', [$this, 'deleteWidgetFromArea']);
+		add_action('widgets.php', [$this, 'addWidgetToArea']);
+//		global $wp_registered_widgets, $wp_registered_widget_controls, $wp_registered_widget_updates;
+//		echo "<pre>";
+//		print_r($wp_registered_widget_updates);
+//		echo "</pre>";
+//		die;
 	}
 
 	public function init()
 	{
 		register_widget('InstafeedHub\Widgets\InstagramFeed');
+	}
+
+	public function deleteWidgetFromArea()
+	{
+
+	}
+
+	public function addWidgetToArea()
+	{?>
+        <script>console.log('aaaaa');</script>
+		<?php
 	}
 }
