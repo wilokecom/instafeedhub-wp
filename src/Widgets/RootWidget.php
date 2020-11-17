f766f039c1d606c271de90564e9e853669d673af
unknown <phucngocdo@gmail.com>
2020-11-17 16:55:40 +0700

    Bỏ text

--- src/Widgets/RootWidget.php
+++ src/Widgets/RootWidget.php
@@ -107,7 +107,7 @@ class RootWidget extends \WP_Widget
 		?>
         <div class="field-item" style="margin-top: 20px">
             <button class="widefat" name="<?php echo $this->get_field_name($fieldKey); ?>" id="<?php echo
-			$this->get_field_id($fieldKey); ?>"><?php echo esc_html__($aArgs['name']); ?></button>
+			$this->get_field_id($fieldKey); ?>"></button>
 			<?php echo $this->printDesc($aArgs); ?>
         </div>
 		<?php
