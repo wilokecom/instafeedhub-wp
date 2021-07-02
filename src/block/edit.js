0157ddb4c3b35927d1664a9f0b1c56efa93556fa
mioriaty <mioriaty@gmail.com>
2021-07-02 17:10:13 +0700

    sua code anh Tuan

--- src/block/edit.js
+++ src/block/edit.js
@@ -1,134 +1,111 @@
-import styled from 'styled-components';
-import { useState, useEffect } from '@wordpress/element';
-import { TextControl, PanelBody, Button, Icon, IconButton } from '@wordpress/components';
-import { InspectorControls } from '@wordpress/editor';
+import { Button, Icon } from '@wordpress/components';
+import { useEffect, useState } from '@wordpress/element';
 import * as _ from 'lodash';
-import { verifyToken, signin } from './actionVeryfiToken';
-import MyModal from './MyModal/MyModal';
-import { INSTA_IFAME_ID, INSTA_IFRAME_URL } from '../modules/contants';
+import { INSTA_IFRAME_URL } from '../modules/contants';
+import { signin, verifyToken } from './actionVeryfiToken';
 
-export default function Edit({ setAttributes, isSelected, attributes, clientId }) {
-	let timeOutRequest;
-
+export default function Edit( { setAttributes, isSelected, attributes, clientId } ) {
 	// === ATTRBUTES === //
 	const { instaTitle, instaId } = attributes;
-
 	// === USESTATE === //
-	const [isVerify, setIsVerify] = useState(false);
-	const [verifyError, setVerifyError] = useState('');
-	const [isOpenModal, setOpenModal] = useState(false);
-	const [isPostMessageDone, setPostMessageDone] = useState(false);
+	const [ isVerify, setIsVerify ] = useState( false );
+	const [ verifyError, setVerifyError ] = useState( '' );
+	const [ isOpenModal, setOpenModal ] = useState( false );
+	const [ postId, setPostId ] = useState( instaId );
 
 	const instaFeedData = () => window.InstafeedHubTokens || {};
 
 	// === HANDLE === //
-	const handleOpenModal = () => setOpenModal(true);
+	const handleOpenModal = () => setOpenModal( true );
 	const handleCloseModal = () => {
-		setOpenModal(false);
-		setPostMessageDone(false);
+		setOpenModal( false );
 	};
 
-	const verifyTokenAndLogin = async () => {
-		if (!instaFeedData() || _.isEmpty(instaFeedData())) {
-			return setVerifyError('Oop! InstafeedHubTokens could not Empty.');
+	const verifyTokenAndLogin = async() => {
+		if ( ! instaFeedData() || _.isEmpty( instaFeedData() ) ) {
+			return setVerifyError( 'Oop! InstafeedHubTokens could not Empty.' );
 		}
-		setIsVerify(true);
+		setIsVerify( true );
 
 		const { accessToken } = instaFeedData();
-		if (!accessToken) {
-			const msg = await signin(instaFeedData());
-			if (typeof msg === 'string' && !!msg) {
-				setVerifyError(msg);
+		if ( ! accessToken ) {
+			const msg = await signin( instaFeedData() );
+			if ( typeof msg === 'string' && !! msg ) {
+				setVerifyError( msg );
 			}
-			if (typeof msg === 'object') {
+			if ( typeof msg === 'object' ) {
 				//  === update lai bien window de nhung component khac su dung luon === //
 				window.InstafeedHubTokens = { ...window.InstafeedHubTokens, ...msg };
 			}
 		} else {
-			const verifyRes = await verifyToken(instaFeedData());
-			console.log(88, { verifyRes });
-			if (typeof verifyRes === 'string' && !!verifyRes) {
-				setVerifyError(verifyRes);
+			const verifyRes = await verifyToken( instaFeedData() );
+			if ( typeof verifyRes === 'string' && !! verifyRes ) {
+				setVerifyError( verifyRes );
 			}
-			if (typeof verifyRes === 'object') {
+			if ( typeof verifyRes === 'object' ) {
 				//  === update lai bien window de nhung component khac su dung luon === //
 				window.InstafeedHubTokens = { ...window.InstafeedHubTokens, ...verifyRes };
 			}
 		}
 
-		setIsVerify(false);
-		return;
+		setIsVerify( false );
 	};
 
-	const handlePostMessage = () => {
-		const iframeWeb = document.getElementById(INSTA_IFAME_ID);
-		if (!iframeWeb) return;
-		const wn = iframeWeb.contentWindow;
-		let payloadData = instaFeedData();
-		if (!!instaId) {
-			payloadData = {
-				...instaFeedData(),
-				args: {
-					id: instaId,
-				},
-			};
+	function handleReceiveFromIframe( event ) {
+		const { type } = event.data;
+		if ( type === 'UPDATE-ITEM' ) {
+			const { payload } = event.data;
+			setPostId( payload.id );
+			setAttributes( {
+				instaId: payload.id,
+				instaTitle: payload.title,
+
+			} );
 		}
-
-		wn.postMessage({ type: 'LOGIN', payload: payloadData }, INSTA_IFRAME_URL);
-		console.log(666, '__HandlePostMessage__', { dataSendPostMessageToIframe: payloadData });
-		setPostMessageDone(true);
-	};
-
-	const onIframeLoaded = () => {
-		if (isPostMessageDone) return;
-		handlePostMessage();
-	};
-
-	function handleReceiveFromIframe(event) {
-		if (window.InstafeedHubClientIdActive !== clientId || !event.data.type) {
-			return;
-		}
-		if (!event.data.type.includes('CREATE-ITEM') && !event.data.type.includes('UPDATE-ITEM')) return;
-		console.log(777, '__handleRecevicePostMessage__', { recevicePostMessage: event.data, isSelected, clientId });
-		if (event.data.payload.status.includes('success')) {
-			setAttributes({
+		if ( type === 'CREATE-ITEM' && event.data.payload.status.includes( 'success' ) ) {
+			setPostId( event.data.payload.id );
+			setAttributes( {
 				instaId: event.data.payload.id,
 				instaTitle: event.data.payload.title,
-			});
-			setTimeout(() => {
+			} );
+			setTimeout( () => {
 				handleCloseModal();
-			}, 500);
+			}, 500 );
 		}
-		return;
 	}
 
 	// === USER EFFECT === //
-	useEffect(() => {
+	useEffect( () => {
 		//
-		window.addEventListener('message', handleReceiveFromIframe);
+		window.addEventListener( 'message', handleReceiveFromIframe );
 		return () => {
-			window.removeEventListener('message', handleReceiveFromIframe);
+			window.removeEventListener( 'message', handleReceiveFromIframe );
 		};
-	}, []);
+	}, [] );
 
 	// === HANDLE === //
 	const handleClickBtnConnect = () => {
 		window.InstafeedHubClientIdActive = clientId;
-		if (!isSelected) return;
+		if ( ! isSelected ) {
+			return;
+		}
 		handleOpenModal();
 		verifyTokenAndLogin();
 	};
 
 	const renderLoading = () => {
-		if (!isVerify) return null;
+		if ( ! isVerify ) {
+			return null;
+		}
 		return (
 			<p className="wilBlogLoading">
-				<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true" focusable="false">
+				<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true"
+					focusable="false">
 					<path
-						fill-rule="evenodd"
+						fillRule="evenodd"
 						d="M10.289 4.836A1 1 0 0111.275 4h1.306a1 1 0 01.987.836l.244 1.466c.787.26 1.503.679 2.108 1.218l1.393-.522a1 1 0 011.216.437l.653 1.13a1 1 0 01-.23 1.273l-1.148.944a6.025 6.025 0 010 2.435l1.149.946a1 1 0 01.23 1.272l-.653 1.13a1 1 0 01-1.216.437l-1.394-.522c-.605.54-1.32.958-2.108 1.218l-.244 1.466a1 1 0 01-.987.836h-1.306a1 1 0 01-.986-.836l-.244-1.466a5.995 5.995 0 01-2.108-1.218l-1.394.522a1 1 0 01-1.217-.436l-.653-1.131a1 1 0 01.23-1.272l1.149-.946a6.026 6.026 0 010-2.435l-1.148-.944a1 1 0 01-.23-1.272l.653-1.131a1 1 0 011.217-.437l1.393.522a5.994 5.994 0 012.108-1.218l.244-1.466zM14.929 12a3 3 0 11-6 0 3 3 0 016 0z"
-						clip-rule="evenodd"
-					></path>
+						clipRule="evenodd"
+					/>
 				</svg>
 				Loading...
 			</p>
@@ -136,35 +113,27 @@ export default function Edit({ setAttributes, isSelected, attributes, clientId }
 	};
 
 	const renderError = () => {
-		if (!verifyError) return null;
-		return <p className="wilBlogError">{verifyError}</p>;
+		if ( ! verifyError ) {
+			return null;
+		}
+		return <p className="wilBlogError">{ verifyError }</p>;
 	};
 
 	const renderModal = () => {
-		if (isVerify) {
+		if ( isVerify ) {
 			return renderLoading();
 		}
-		if (!!verifyError) {
+		if ( !! verifyError ) {
 			return renderError();
 		}
-		window.open(INSTA_IFRAME_URL);
-		return 'Ok';
-		// return (
-		// 	<MyModal onCloseModal={handleCloseModal}>
-		// 		{isVerify ? (
-		// 			renderLoading()
-		// 		) : !!verifyError ? (
-		// 			renderError()
-		// 		) : (
-		// 			<iframe onLoad={onIframeLoaded} id={INSTA_IFAME_ID} src={INSTA_IFRAME_URL} frameBorder="0" />
-		// 		)}
-		// 	</MyModal>
-		// );
+		window.open( INSTA_IFRAME_URL );
+		return <span>{ postId }</span>;
 	};
 
 	const renderIconSvg = () => {
 		return (
-			<svg enable-background="new 0 0 24 24" height="30" viewBox="0 0 24 24" width="30" xmlns="http://www.w3.org/2000/svg">
+			<svg enableBackground="new 0 0 24 24" height="30" viewBox="0 0 24 24" width="30"
+				xmlns="http://www.w3.org/2000/svg">
 				<linearGradient
 					id="SVGID_1_"
 					gradientTransform="matrix(0 -1.982 -1.844 0 -132.522 -51.077)"
@@ -174,9 +143,9 @@ export default function Edit({ setAttributes, isSelected, attributes, clientId }
 					y1="-72.705"
 					y2="-84.047"
 				>
-					<stop offset="0" stop-color="#fd5" />
-					<stop offset=".5" stop-color="#ff543e" />
-					<stop offset="1" stop-color="#c837ab" />
+					<stop offset="0" stopColor="#fd5" />
+					<stop offset=".5" stopColor="#ff543e" />
+					<stop offset="1" stopColor="#c837ab" />
 				</linearGradient>
 				<path
 					d="m1.5 1.633c-1.886 1.959-1.5 4.04-1.5 10.362 0 5.25-.916 10.513 3.878 11.752 1.497.385 14.761.385 16.256-.002 1.996-.515 3.62-2.134 3.842-4.957.031-.394.031-13.185-.001-13.587-.236-3.007-2.087-4.74-4.526-5.091-.559-.081-.671-.105-3.539-.11-10.173.005-12.403-.448-14.41 1.633z"
@@ -189,16 +158,16 @@ export default function Edit({ setAttributes, isSelected, attributes, clientId }
 			</svg>
 		);
 	};
-
 	const iconName = instaId ? 'edit' : 'welcome-add-page';
 	return (
 		<div className="wilIntaFeedCp">
-			<div>{renderIconSvg()}</div>
-			<Button isPrimary onClick={handleClickBtnConnect} className="wilIntaFeedCp__btnEdit" icon={<Icon icon={iconName} />}>
-				{verifyError}
-				{!instaId ? 'Connect and Create new a InstaFeed' : `${instaTitle} (${instaId})`}
+			<div>{ renderIconSvg() }</div>
+			<Button isPrimary onClick={ handleClickBtnConnect } className="wilIntaFeedCp__btnEdit"
+				icon={ <Icon icon={ iconName } /> }>
+				{ verifyError }
+				{ ! postId ? 'Connect and Create new a InstaFeed' : `${ instaTitle } (${ postId })` }
 			</Button>
-			{isOpenModal && renderModal()}
+			{ isOpenModal && renderModal() }
 		</div>
 	);
 }
--- src/block/edit_copy.js
+++ src/block/edit_copy.js
@@ -1,141 +1,137 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import { useEffect, useState } from 'react';
-import { TextControl, Autocomplete, PanelBody } from "@wordpress/components";
-import { InspectorControls } from "@wordpress/editor";
-import axios from "axios";
-import * as _ from "lodash";
-import MyAutocomplete from "../components/AutoComplete/AutoComplete";
-import { verifyToken, signin } from "./actionVeryfiToken";
+import { TextControl, Autocomplete, PanelBody } from '@wordpress/components';
+import { InspectorControls } from '@wordpress/editor';
+import axios from 'axios';
+import * as _ from 'lodash';
+import MyAutocomplete from '../components/AutoComplete/AutoComplete';
+import { verifyToken, signin } from './actionVeryfiToken';
 
-export default function Edit ({ setAttributes, isSelected, attributes }) {
-    //   === CONFIG AXIOS CANCEL ===	//
-    const CancelToken = axios.CancelToken;
-    let cancel;
+export default function Edit( { setAttributes, isSelected, attributes } ) {
+	//   === CONFIG AXIOS CANCEL ===	//
+	const CancelToken = axios.CancelToken;
+	let cancel;
 
-    let timeOutRequest;
+	let timeOutRequest;
 
-    // === ATTRBUTES === //
-    const { options = [], value = "" } = attributes;
+	// === ATTRBUTES === //
+	const { options = [], value = '' } = attributes;
 
-    const [isVerify, setIsVerify] = useState(false);
-    const [verifyError, setVerifyError] = useState('');
-    // === USESTATE === //
+	const [ isVerify, setIsVerify ] = useState( false );
+	const [ verifyError, setVerifyError ] = useState( '' );
+	// === USESTATE === //
 
-  const verifyTokenAndLogin = async () => {
-    // window.InstafeedHubTokens = {
-    //     accessToken?: string;
-    //     refreshToken?: string;
-    //     email: string;
-    //     nickname: string;
-            // origin: string;
-    //     args: {
-    //       id?: string;
-    //     };
-    //   };
+	const verifyTokenAndLogin = async() => {
+		// window.InstafeedHubTokens = {
+		//     accessToken?: string;
+		//     refreshToken?: string;
+		//     email: string;
+		//     nickname: string;
+		// origin: string;
+		//     args: {
+		//       id?: string;
+		//     };
+		//   };
 
+		if ( ! window.InstafeedHubTokens ) {
+			return setVerifyError( 'Oop! InstafeedHubTokens could not Empty.' );
+		}
 
-    if (!window.InstafeedHubTokens) {
-        return setVerifyError('Oop! InstafeedHubTokens could not Empty.');
-    }
+		const instafeedHubTokens = window.InstafeedHubTokens;
+		const {
+			accessToken, refreshToken, email, nickname, args, origin,
+		} = instafeedHubTokens;
 
-    const instafeedHubTokens = window.InstafeedHubTokens;
-    const {
-        accessToken,refreshToken,email,nickname, args, origin
-    } = instafeedHubTokens;
+		setIsVerify( true );
 
-    setIsVerify(true);
+		if ( !! accessToken ) {
+			// === USER da~ login roi === //
+			const msg = await verifyToken( instafeedHubTokens );
+			msg && setVerifyError( msg );
+		} else {
+			const msg = await signin( origin, email, nickname, args );
+			msg && setVerifyError( msg );
+		}
 
-    if (!!accessToken) {
-        // === USER da~ login roi === //
-      const msg = await verifyToken(instafeedHubTokens);
-      msg && setVerifyError(msg);
-    } else {
-      const msg = await signin(origin, email, nickname, args);
-      msg && setVerifyError(msg);
-    }
+		setIsVerify( false );
+	};
 
-    setIsVerify(false);
-    return;
-  };
+	// === USE EFFECT === //
+	useEffect( () => {
+		verifyTokenAndLogin();
+	}, [] );
 
-    // === USE EFFECT === //
-    useEffect(() => {
-        verifyTokenAndLogin()
-    }, [])
+	const actionAxiosFindUser = ( query ) => {
+		const url = `https://shopifyinstagram.io:7890/wp-json/wiloke/v1/instagram/me/find-insta-user?q=${ query }`;
+		if ( cancel ) {
+			cancel();
+		}
+		axios
+			.get( url, {
+				responseType: 'json',
+				cancelToken: new CancelToken( ( c ) => {
+					cancel = c;
+				} ),
+			} )
+			.then( ( { data } ) => {
+				setAttributes( {
+					options: data.items || [],
+					// options: _.unionWith(data.items, options, _.isEqual),
+				} );
+			} )
+			.catch( ( thrown ) => {
+				if ( axios.isCancel( thrown ) ) {
+					console.log( 'Request canceled', thrown.message );
+				} else {
+					console.log( 'ERROR', { thrown } );
+				}
+			} );
+	};
 
+	function findInstaUserId( val ) {
+		timeOutRequest && clearTimeout( timeOutRequest );
+		if ( !! val ) {
+			timeOutRequest = setTimeout( () => {
+				actionAxiosFindUser( val );
+			}, 300 );
+		}
+	}
 
+	function downloadInstaConfigured( { id } ) {
+		const url = `https://shopifyinstagram.io:7890/wp-json/wiloke/v1/instagram/me/${ id }/global-configuration`;
+		axios
+			.get( url, {
+				responseType: 'json',
+			} )
+			.then( console.log )
+			.catch( console.error );
+	}
 
-    const actionAxiosFindUser = (query) => {
-      const url = `https://shopifyinstagram.io:7890/wp-json/wiloke/v1/instagram/me/find-insta-user?q=${query}`;
-      if (cancel) {
-        cancel();
-      }
-      axios
-        .get(url, {
-          responseType: "json",
-          cancelToken: new CancelToken((c) => {
-            cancel = c;
-          }),
-        })
-        .then(({ data }) => {
-          setAttributes({
-            options: data.items || [],
-            // options: _.unionWith(data.items, options, _.isEqual),
-          });
-        })
-        .catch((thrown) => {
-          if (axios.isCancel(thrown)) {
-            console.log("Request canceled", thrown.message);
-          } else {
-            console.log("ERROR", { thrown });
-          }
-        });
-    };
+	console.log( 44, { attributes } );
 
-    function findInstaUserId(val) {
-      timeOutRequest && clearTimeout(timeOutRequest);
-      if (!!val) {
-        timeOutRequest = setTimeout(() => {
-          actionAxiosFindUser(val);
-        }, 300);
-      }
-    }
+	if ( isVerify ) {
+		return <p className="wilBlogLoading" >
+			<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true" focusable="false"><path fillRule="evenodd" d="M10.289 4.836A1 1 0 0111.275 4h1.306a1 1 0 01.987.836l.244 1.466c.787.26 1.503.679 2.108 1.218l1.393-.522a1 1 0 011.216.437l.653 1.13a1 1 0 01-.23 1.273l-1.148.944a6.025 6.025 0 010 2.435l1.149.946a1 1 0 01.23 1.272l-.653 1.13a1 1 0 01-1.216.437l-1.394-.522c-.605.54-1.32.958-2.108 1.218l-.244 1.466a1 1 0 01-.987.836h-1.306a1 1 0 01-.986-.836l-.244-1.466a5.995 5.995 0 01-2.108-1.218l-1.394.522a1 1 0 01-1.217-.436l-.653-1.131a1 1 0 01.23-1.272l1.149-.946a6.026 6.026 0 010-2.435l-1.148-.944a1 1 0 01-.23-1.272l.653-1.131a1 1 0 011.217-.437l1.393.522a5.994 5.994 0 012.108-1.218l.244-1.466zM14.929 12a3 3 0 11-6 0 3 3 0 016 0z" clipRule="evenodd"></path></svg>
+			Loading...
+		</p>;
+	}
+	if ( !! verifyError ) {
+		return <p className="wilBlogError">{ verifyError }</p>;
+	}
 
-    function downloadInstaConfigured({ id }) {
-      const url = `https://shopifyinstagram.io:7890/wp-json/wiloke/v1/instagram/me/${id}/global-configuration`;
-      axios
-        .get(url, {
-          responseType: "json",
-        })
-        .then(console.log)
-        .catch(console.error);
-    }
-
-    console.log(44, { attributes });
-
-    if(isVerify) {
-        return <p className="wilBlogLoading" >
-            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true" focusable="false"><path fill-rule="evenodd" d="M10.289 4.836A1 1 0 0111.275 4h1.306a1 1 0 01.987.836l.244 1.466c.787.26 1.503.679 2.108 1.218l1.393-.522a1 1 0 011.216.437l.653 1.13a1 1 0 01-.23 1.273l-1.148.944a6.025 6.025 0 010 2.435l1.149.946a1 1 0 01.23 1.272l-.653 1.13a1 1 0 01-1.216.437l-1.394-.522c-.605.54-1.32.958-2.108 1.218l-.244 1.466a1 1 0 01-.987.836h-1.306a1 1 0 01-.986-.836l-.244-1.466a5.995 5.995 0 01-2.108-1.218l-1.394.522a1 1 0 01-1.217-.436l-.653-1.131a1 1 0 01.23-1.272l1.149-.946a6.026 6.026 0 010-2.435l-1.148-.944a1 1 0 01-.23-1.272l.653-1.131a1 1 0 011.217-.437l1.393.522a5.994 5.994 0 012.108-1.218l.244-1.466zM14.929 12a3 3 0 11-6 0 3 3 0 016 0z" clip-rule="evenodd"></path></svg>
-            Loading...
-        </p>
-    }
-    if(!!verifyError) {
-        return <p className="wilBlogError">{verifyError}</p>
-    };
-
-    return (
-      <div>
-        <MyAutocomplete
-          label="Search your instagram"
-          onChange={findInstaUserId}
-          onSelectItem={(item) => {
-            setAttributes({ value: item.label });
-            downloadInstaConfigured(item);
-          }}
-          options={options}
-          id="block-id"
-          value={value}
-        />
-      </div>
-    );
-  }
+	return (
+		<div>
+			<MyAutocomplete
+				label="Search your instagram"
+				onChange={ findInstaUserId }
+				onSelectItem={ ( item ) => {
+					setAttributes( { value: item.label } );
+					downloadInstaConfigured( item );
+				} }
+				options={ options }
+				id="block-id"
+				value={ value }
+			/>
+		</div>
+	);
+}
